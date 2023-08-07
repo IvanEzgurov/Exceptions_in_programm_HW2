1ac8d6f0fdd99ec420b0aae921087138ff2cd87a
Ivan_Ezgurov <ezgurovivan@gmail.com>
2023-08-07 16:38:46 +0300

    HW2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import task.task1;
+import task.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        task1 ex1 = new task1();
+        ex1.method();
+
+        task2 ex2 = new task2();
+        ex2.method();
+    }
+}
